6469f62cd28c71fe3745379118c5d3b6ca1c8e08
AP-BAPPI-WEBAPP\bappi.chandra <bappi.chandra@amiprobashi.com>
2025-03-12 00:31:49 +0600

    Update Array Elements and Fill Array

---
+++ Array/03 - Update Array Elements.js
@@ -0,0 +1,48 @@
+/**
+ * Update Array Elements and Fill Array
+ */
+
+// Array Fill Manual Way
+const arr1 = new Array(10);
+for (let i = 0; i < arr1.length; i++) {
+    arr1[i] = false;
+}
+console.log(arr1);
+
+// Array Fill
+const arr2 = new Array(10);
+arr2.fill(0);
+console.log(arr2);
+
+// Arrau Update
+const names = ['Bappy', 'Rakib', 'Jahangir'];
+names[0] = 'Ch Bappy';
+names[1] = 'Saful Alom Rakib';
+names[2] = 'Jahangir Alom';
+console.log(names);
+
+// Fill Array and Update
+const response = new Array(9);
+response.fill(false);
+for(let i = 0; i < response.length; i++){
+    const rand = Math.floor(Math.random() * 10 +1);
+    response[i] = rand > 5 ? 'X' : 'O';
+}
+console.log(response);
+
+// Array is Mutable
+function update(arr) {
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = `${i + 1}. ${arr[i]}`;
+    }
+    return arr;
+}
+const updatedNames = update(names);
+console.log(names);
+console.log(updatedNames);
+console.log(names === updatedNames);
+
+
+
+
+
